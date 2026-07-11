e17095b2b883755ebb7797a64d53129c904c4a96
repinek <137826826+repinek@users.noreply.github.com>
2026-07-11 22:30:10 +0500

    refactor: remove ts-ignores, move logic to own functions

--- src/modules/Ship.ts
+++ src/modules/Ship.ts
@@ -15,8 +15,6 @@ export class ShipModule extends BaseModule {
 
     private ShipStatus_FixedUpdate!: Il2Cpp.Method;
 
-    private LocalPlayer?: Il2Cpp.Field;
-
     public init(): void {
         this.HudManager = AssemblyHelper.AssemblyCSharp.class("HudManager");
         this.MapOptions = AssemblyHelper.AssemblyCSharp.class("MapOptions");
@@ -25,35 +23,33 @@ export class ShipModule extends BaseModule {
         this.VentilationSystem = AssemblyHelper.AssemblyCSharp.class("VentilationSystem");
 
         this.ShipStatus_FixedUpdate = this.ShipStatus.method<void>("FixedUpdate");
-
-        this.LocalPlayer = this.PlayerControl.field("LocalPlayer");
     }
 
     public override initHooks(): void {
         const module = this;
 
         this.ShipStatus_FixedUpdate.implementation = function (): void {
-            if (State.walkInVents) {
-                // @ts-ignore
-                const localPlayer = module.LocalPlayer.value as Il2Cpp.Object;
+            const returnValue = this.method<void>("FixedUpdate").invoke();
 
-                if (localPlayer.isNull()){
-                    return this.method<void>("FixedUpdate").invoke();
+            if (State.walkInVents) {
+                const localPlayer = module.localPlayer;
+
+                if (localPlayer.isNull()) {
+                    return returnValue; 
                 }
 
-                localPlayer.field("inVent").value = false;
+                localPlayer.field<boolean>("inVent").value = false;
                 localPlayer.field("moveable").value = true;
             }
 
-            return this.method<void>("FixedUpdate").invoke();
+            return returnValue; 
         };
     }
 
     public callMeeting(): void {
         const module = this;
 
-        // @ts-ignore
-        const localPlayer = module.LocalPlayer.value as Il2Cpp.Object;
+        const localPlayer = module.localPlayer;
         if (localPlayer.isNull()){
             Logger.warn(`[${module.name}::callMeeting] LocalPlayer is null`);
             return;
@@ -65,7 +61,7 @@ export class ShipModule extends BaseModule {
     public sabotageReactor(): void {
         const module = this;
 
-        const ShipStatusInstance = module.ShipStatus.field("Instance").value as Il2Cpp.Object;
+        const ShipStatusInstance = module.ShipStatus.field<Il2Cpp.Object>("Instance").value;
         if (ShipStatusInstance.isNull()) {
             Logger.warn(`[${module.name}::sabotageReactor] ShipStatusInstance is null`);
             return;
@@ -77,7 +73,7 @@ export class ShipModule extends BaseModule {
     public kickVents(): void {
         const module = this;
 
-        const ShipStatusInstance = module.ShipStatus.field("Instance").value as Il2Cpp.Object;
+        const ShipStatusInstance = module.ShipStatus.field<Il2Cpp.Object>("Instance").value;
         if (ShipStatusInstance.isNull()) {
             Logger.warn(`[${module.name}::kickVents] ShipStatusInstance is null`);
             return;
@@ -86,8 +82,8 @@ export class ShipModule extends BaseModule {
         const allVents = ShipStatusInstance.method<Il2Cpp.Array<Il2Cpp.Object>>("get_AllVents").invoke();
 
         for (const vent of allVents) {
-            module.VentilationSystem.method<void>("Update", 2).invoke(5, vent.field("Id").value);
             // VentilationSystem.Operation.BootImpostors = 5
+            module.VentilationSystem.method<void>("Update", 2).invoke(5, vent.field("Id").value);
         }
     }
 
@@ -106,14 +102,19 @@ export class ShipModule extends BaseModule {
         //     Mode = MapOptions.Modes.Sabotage
         // });
 
-        //const options = module.MapOptions;
-        const opt = UnityUtils.createInstance(module.MapOptions) as Il2Cpp.Object;
-        opt.field("Mode").value = 3;
-        //const options = Il2Cpp.gc.choose(module.MapOptions);
-        //options.field("Mode").value = 3;
-
-        // @ts-ignore
-        HudManagerInstance.method<void>("ToggleMapVisible", [module.MapOptions]).invoke(opt);
+        const opt = UnityUtils.createInstance(module.MapOptions);
         // MapOptions.Modes.Sabotage = 3
+        opt.field("Mode").value = 3;
+
+        HudManagerInstance.method<void>("ToggleMapVisible", 1).invoke(opt);
+    }
+
+    /** 
+     * `static PlayerControl::LocalPlayer` 
+     * 
+     * @returns `PlayerControl` instance
+    */
+    private get localPlayer(): Il2Cpp.Object {
+        return this.PlayerControl.field<Il2Cpp.Object>("LocalPlayer").value;
     }
 }
